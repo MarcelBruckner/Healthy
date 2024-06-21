e6ea5305d4419908899655eddc43faeb28d46962
Marcel Bruckner <bruckner@cqse.eu>
2024-06-21 13:25:57 +0200

    Better 404 handling

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 import SideNav from "@/app/ui/dashboard/sidenav";
 import { Box } from "@mui/material";
-import Toolbar from '@mui/material/Toolbar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
--- app/dashboard/not-found.tsx
+++ app/dashboard/not-found.tsx
@@ -1,5 +1,13 @@
 import NotFoundBase from "@/app/ui/dashboard/not-found";
+import { Box } from "@mui/material";
+import SideNav from "@/app/ui/dashboard/sidenav";
 
 export default function NotFound() {
-    return <NotFoundBase message="Essen und Trinken nicht gefunden!" href="/dashboard/food" />
+    return (
+        <SideNav>
+            <Box padding={2}>
+                <NotFoundBase message="Resource nicht gefunden!" href="/dashboard" />
+            </Box>
+        </SideNav>
+    );
 }
